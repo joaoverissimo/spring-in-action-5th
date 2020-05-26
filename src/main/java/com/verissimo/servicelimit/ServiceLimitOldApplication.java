771d48cbcf71975b96f711f0fdaa2411419a9056
Verissimo Ribeiro <vribeiro@landytech.com>
2020-05-26 23:05:40 +0100

    start project

---
+++ src/main/java/com/verissimo/servicelimit/ServiceLimitOldApplication.java
@@ -0,0 +1,13 @@
+package com.verissimo.servicelimit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceLimitOldApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ServiceLimitOldApplication.class, args);
+  }
+
+}
