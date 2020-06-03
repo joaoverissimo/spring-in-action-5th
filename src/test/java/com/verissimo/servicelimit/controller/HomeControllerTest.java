3065c0e2e53a4b5702476749c40fa702d17ca9ac
verissimo ribeiro <joao-verissimo@hotmail.com>
2020-06-04 00:43:02 +0100

    move home controller to webconfig

--- src/test/java/com/verissimo/servicelimit/controller/HomeControllerTest.java
+++ src/test/java/com/verissimo/servicelimit/controller/HomeControllerTest.java
@@ -1,21 +1,14 @@
 package com.verissimo.servicelimit.controller;
 
-import org.assertj.core.internal.bytebuddy.matcher.StringMatcher;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
-import org.mockito.internal.hamcrest.HamcrestArgumentMatcher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-@WebMvcTest(HomeController.class)
+@WebMvcTest
 class HomeControllerTest {
 
   @Autowired
